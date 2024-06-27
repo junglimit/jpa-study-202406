d4c89946f96102107be471b3879bb79c33ca3bfb
junglimit <jaehan_jjh@naver.com>
2024-06-27 10:58:01 +0900

    JPA 다대다 관계 시나리오 테스트

---
+++ src/main/java/com/spring/jpastudy/chap05/entity/User.java
@@ -0,0 +1,32 @@
+package com.spring.jpastudy.chap05.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_mtm_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    // mappedBy 엔 꼭 필드명을 넣기
+    @OneToMany(mappedBy = "user",orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Purchase> purchaseList = new ArrayList<>();
+
+}
