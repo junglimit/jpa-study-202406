15404de837b30d38a78fb1940d6bf10ed2471905
junglimit <jaehan_jjh@naver.com>
2024-06-28 11:03:33 +0900

    gropupBy, having 기본

--- src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/entity/Idol.java
@@ -22,14 +22,27 @@ public class Idol {
 
     private int age;
 
+    private String gender; // 성별 추가
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
 
 
+
+
     public Idol(String idolName, int age, Group group) {
         this.idolName = idolName;
         this.age = age;
+        if (group != null) {
+            changeGroup(group);
+        }
+    }
+
+    public Idol(String idolName, int age,String gender, Group group) {
+        this.idolName = idolName;
+        this.age = age;
+        this.gender = gender;
         if (group != null) {
             changeGroup(group);
         }
