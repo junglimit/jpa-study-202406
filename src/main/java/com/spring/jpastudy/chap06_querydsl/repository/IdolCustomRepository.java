6ed64bbb293a85df0d3f02d23b5fb3f1fb0817a6
junglimit <jaehan_jjh@naver.com>
2024-06-28 10:35:13 +0900

    커스텀 레포지토리 사용하여 정렬하기

--- src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolCustomRepository.java
+++ src/main/java/com/spring/jpastudy/chap06_querydsl/repository/IdolCustomRepository.java
@@ -1,15 +1,20 @@
 package com.spring.jpastudy.chap06_querydsl.repository;
 
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IdolCustomRepository {
 
+    // JPA 의 PAGE 인터네페이스를 사용
+    Page<Idol> foundAllByPaging(Pageable pageable);
+
     // 이름으로 오름차해서 전체조회
-    List<Idol> findAllSortedByName();
+    List<Idol> foundAllSortedByName2();
 
     // 그룹명으로 아이돌을 조회
-    List<Idol> findByGroupName();
+    List<Idol> foundByGroupName();
 
 }
