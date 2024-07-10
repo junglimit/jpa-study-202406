b18b1cffda18676f8f39d255511935e0668f9b4e
junglimit <jaehan_jjh@naver.com>
2024-07-10 14:41:10 +0900

    JPA 복습

--- src/main/java/com/spring/jpastudy/event/controller/EventController.java
+++ src/main/java/com/spring/jpastudy/event/controller/EventController.java
@@ -1,7 +1,8 @@
 package com.spring.jpastudy.event.controller;
 
 import com.spring.jpastudy.event.dto.request.EventSaveDto;
-import com.spring.jpastudy.event.entity.Event;
+import com.spring.jpastudy.event.dto.response.EventDetailDto;
+import com.spring.jpastudy.event.dto.response.EventOneDto;
 import com.spring.jpastudy.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,16 +22,36 @@ public class EventController {
 
     // 전체 조회 요청
     @GetMapping
-    public ResponseEntity<?> getList(String sort) {
-        List<Event> events = eventService.getEvents(sort);
+    public ResponseEntity<?> getList(
+            @RequestParam(required = false) String sort) {
+
+        if (sort == null) {
+            return ResponseEntity.badRequest().body("sort 파라미터가 없습니다.");
+        }
+
+        List<EventDetailDto> events = eventService.getEvents(sort);
         return ResponseEntity.ok().body(events);
     }
 
     // 등록 요청
     @PostMapping
     public ResponseEntity<?> register(@RequestBody EventSaveDto dto) {
-        List<Event> events = eventService.saveEvent(dto);
+        List<EventDetailDto> events = eventService.saveEvent(dto);
         return ResponseEntity.ok().body(events);
     }
 
+    // 단일 조회 요청
+    @GetMapping("/{eventId}")
+    public ResponseEntity<?> getEvent(@PathVariable Long eventId) {
+
+        if (eventId == null || eventId < 1) {
+            String errorMessage = "eventId가 정확하지 않습니다.";
+            log.warn(errorMessage);
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        EventOneDto eventDetail = eventService.getEventDetail(eventId);
+
+        return ResponseEntity.ok().body(eventDetail);
+    }
 }
--- src/main/java/com/spring/jpastudy/event/service/EventService.java
+++ src/main/java/com/spring/jpastudy/event/service/EventService.java
@@ -1,6 +1,8 @@
 package com.spring.jpastudy.event.service;
 
 import com.spring.jpastudy.event.dto.request.EventSaveDto;
+import com.spring.jpastudy.event.dto.response.EventDetailDto;
+import com.spring.jpastudy.event.dto.response.EventOneDto;
 import com.spring.jpastudy.event.entity.Event;
 import com.spring.jpastudy.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +22,27 @@ public class EventService {
     private final EventRepository eventRepository;
 
     // 전체 조회 서비스
-    public List<Event> getEvents(String sort) {
-        return eventRepository.findEvents(sort);
+    public List<EventDetailDto> getEvents(String sort) {
+        return eventRepository.findEvents(sort)
+                .stream().map(EventDetailDto::new)
+                .collect(Collectors.toList())
+                ;
     }
 
     // 이벤트 등록
-    public List<Event> saveEvent(EventSaveDto dto) {
+    public List<EventDetailDto> saveEvent(EventSaveDto dto) {
         Event savedEvent = eventRepository.save(dto.toEntity());
         log.info("saved event: {}", savedEvent);
         return getEvents("date");
     }
+
+    // 이벤트 단일 조회
+    public EventOneDto getEventDetail(Long id) {
+
+        Event foundEvent = eventRepository.findById(id).orElseThrow();
+
+        return new EventOneDto(foundEvent);
+    }
+
+
 }
